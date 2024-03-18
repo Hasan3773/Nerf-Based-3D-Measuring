1234767aa05ebb2f2dff4f4d48de352006d2748c
Owen Leather <owenleather03@gmail.com>
2024-03-18 17:27:32 -0400

    made colour tuning use continuous video feed

--- colour_tuning.py
+++ colour_tuning.py
@@ -8,11 +8,6 @@ RED_MIN = np.array([106, 141, 63])
 print(RED_MAX)
 print(RED_MIN)
 
-src = cv2.imread("datasets/WIN_20240318_17_10_33_Pro.jpg")
-
-image = cv2.cvtColor(src, cv2.COLOR_RGB2HSV)
-filtered_mask = cv2.inRange(image, RED_MIN, RED_MAX)
-
 # trackbar
 def nothing(x):
     pass
@@ -29,7 +24,26 @@ cv2.createTrackbar('H2', 'filtered', 0, 180, nothing)
 cv2.createTrackbar('S2', 'filtered', 0, 255, nothing)
 cv2.createTrackbar('V2', 'filtered', 0, 255, nothing)
 
+use_camera = True
+
+if use_camera:        
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+else: 
+    frame = cv2.imread("datasets/WIN_20240318_17_10_33_Pro.jpg")
+
 while(1):
+    print('t')
+    if use_camera:
+        ret, frame = cap.read()
+        print('t')
+    
+    image = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+
     # get current positions of four trackbars
     h1 = cv2.getTrackbarPos('H1','filtered')
     s1 = cv2.getTrackbarPos('S1','filtered')
@@ -45,8 +59,7 @@ while(1):
 
     # recreate the mask
     filtered_mask = cv2.inRange(image, RED_MIN, RED_MAX)
-    masked_rgb =  cv2.bitwise_and(image, image, mask=filtered_mask)
+    masked_rgb = cv2.bitwise_and(frame, frame, mask=filtered_mask)
     masked_rgb = np.concatenate([masked_rgb, filtered_mask[..., None]], axis=2)
-    masked_rgb = cv2.cvtColor(masked_rgb, cv2.COLOR_BGR2RGB)
     cv2.imshow('filtered', masked_rgb)
     cv2.waitKey(1)
